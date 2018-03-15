d60e4d6feed1fba4a6d9d5ab3454bd4b86185056
aahmad94 <aka5vm@virginia.edu>
2018-03-15 00:33:54 -0700

    add jwt in auth controller

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -1,10 +1,17 @@
 const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
 
-  if (!email || !password) {
+  if (!email || !password)  {
     return res.status(422).send({ error: 'You must provide email and password' });
   }
   
@@ -29,7 +36,7 @@ exports.signup = function(req, res, next) {
 
       // Respond to request indicating the user was created
       res.json({
-        success: true
+        token: tokenForUser(user)
       });
     });
   });
