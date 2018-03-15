795924c63a16db2864a303b025f828901d8866a3
aahmad94 <aka5vm@virginia.edu>
2018-03-15 16:18:17 -0700

    implement jwt validation in router

--- auth/server/services/passport.js
+++ auth/server/services/passport.js
@@ -1,12 +1,15 @@
 const passport = require('passport');
 const User = require('../models/user');
-const config = require('config');
+const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local strategy
 
 // Setup options for JWT Strategy
 const jwtOptions = {
-  jstFromRequest: ExtractJwt.fromHeader('authorization'),
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
   secretOrKey: config.secret
 };
 
